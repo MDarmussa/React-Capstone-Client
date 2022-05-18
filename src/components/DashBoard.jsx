96abd960059fb7688af5758e09653b618bb1ad17
Kristen Bartel <kristenbartel@gmail.com>
2022-05-18 14:23:38 -0500

    app clean up and comps added

---
+++ src/components/DashBoard.jsx
@@ -0,0 +1,22 @@
+import { Expense } from "./Expense";
+import ExpenseForm from "./ExpenseForm";
+import { LatestOrders } from './LatestOrders';
+import { Sample } from './Sample';
+
+
+const DashBoard = () => {
+    return ( 
+        <>
+        <ExpenseForm />
+        <h1>DashBoard</h1>
+        
+        <Expense />
+        
+        <Sample></Sample>
+      <LatestOrders></LatestOrders>
+       
+        </>
+     );
+}
+ 
+export default DashBoard;
---
+++ src/components/ExpenseForm.jsx
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import { MenuItem } from '@mui/material';
+
+
+const currencies = [
+    {
+      value: 'USD',
+      label: '$',
+    },
+    {
+      value: 'EUR',
+      label: '€',
+    },
+    {
+      value: 'BTC',
+      label: '฿',
+    },
+    {
+      value: 'JPY',
+      label: '¥',
+    },
+  ];
+
+
+
+
+export default function ExpenseForm() {
+    const [currency, setCurrency] = React.useState('EUR');
+
+    const handleChange = (event) => {
+      setCurrency(event.target.value);
+    };
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Payment method
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="cardName"
+            label="Name on card"
+            fullWidth
+            autoComplete="cc-name"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="cardNumber"
+            label="Card number"
+            fullWidth
+            autoComplete="cc-number"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+        <TextField
+          id="outlined-select-currency"
+          select
+          label="Select"
+          value={currency}
+          onChange={handleChange}
+          helperText="Please select your currency"
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="cvv"
+            label="CVV"
+            helperText="Last three digits on signature strip"
+            fullWidth
+            autoComplete="cc-csc"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
+            label="Remember credit card details for next time"
+          />
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+}
