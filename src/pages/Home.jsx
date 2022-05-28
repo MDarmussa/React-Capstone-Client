3e23feff61b8026435df0da11c6fe5465392f796
rashida-otunba <rashidaotunba@gmail.com>
2022-05-27 20:37:04 -0400

    downloaded fonts from google; updated main page and nav bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,31 @@
-import { Button } from '@mui/material'
- 
+import { Button } from "@mui/material";
+import "./Home.css"
+
 function Home() {
-    return (
-        <>
-            <div>
-                <h1>Spence</h1>
-                <h3>Manage you Wallet with just a few clicks</h3>
-                <Button href='register' variant="outlined">get balanced</Button>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <main className="home-container">
+        <div className="main-container">
+          <div>
+            <h1 className="main-title">Spence</h1>
+          </div>
+          <div>
+            <h2>Manage your wallet with just a few clicks</h2>
+          </div>
+          <div>
+            <Button className="main-button" href="register" variant="unoutlined"  
+            sx={{ 
+                fontSize: 14, 
+                backgroundColor: '#FBCD8A', 
+                color: '#162B1E', 
+                fontFamily:" Libre Bodoni", }}
+            >
+              get balanced.
+            </Button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
-export default Home
+export default Home;
